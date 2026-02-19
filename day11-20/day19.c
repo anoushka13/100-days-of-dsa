eca68fe0e4ae6e7b97cfea30bd5619fffe477bca
anoushka chawla <anoushka.22992@stu.upes.ac.in>
2026-02-20 00:03:35 +0530

    Create day19.c

---
+++ day11-20/day19.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    if(n < 2) {
+        printf("At least two elements are required.");
+        return 0;
+    }
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    qsort(arr, n, sizeof(int), compare);
+
+    int left = 0;
+    int right = n - 1;
+
+    int minSum = INT_MAX;
+    int first = 0, second = 0;
+
+    
+    while(left < right) {
+        int sum = arr[left] + arr[right];
+
+        if(abs(sum) < abs(minSum)) {
+            minSum = sum;
+            first = arr[left];
+            second = arr[right];
+        }
+
+        if(sum < 0)
+            left++;
+        else
+            right--;
+    }
+
+    printf("Pair closest to zero: %d %d\n", first, second);
+
+    return 0;
+}
